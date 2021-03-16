ab442e29564bd3a47705462d932f701d1e9d51bd
wanghusheng <2095606168@qq.com>
2021-03-16 15:02:11 +0800

    version-1.0

---
+++ src/com/msu/domain/Student.java
@@ -0,0 +1,28 @@
+package com.msu.domain;
+
+/**
+ * @author 王沪生
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/3/16 14:20
+ */
+public class Student {
+    private Integer age;
+    private String name;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
